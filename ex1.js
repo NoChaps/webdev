7d9682af3d9e7e989efb1a53cdc6d2e68e7bd86d
NoChaps <vkempiak@gmail.com>
2023-03-02 16:09:17 -0500

    git commit

--- ex1.js
+++ ex1.js
@@ -1,1 +1,49 @@
-let eggs = [Ada]
+let output = [];
+let x = 1;
+function fizzBuzz() {
+
+    if (x % 3 === 0 && x % 5 === 0) {
+        output.push("FizzBuzz");
+        x++;
+        console.log(output);
+    } else if (x % 3 === 0) {
+        output.push("Fizz");
+        x++;
+        console.log(output);
+    } else if (x % 5 === 0) {
+        output.push("Buzz");
+        x++;
+        console.log(output);
+    } else {
+        output.push(x);
+        x++;
+        console.log(output);
+    }
+}
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
