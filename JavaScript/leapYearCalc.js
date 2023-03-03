6cb6c7eb2dfe32aef97e78ccf48b6c8d04c6ca13
NoChaps <vkempiak@gmail.com>
2023-03-02 20:15:24 -0500

    git commit

--- JavaScript/leapYearCalc.js
+++ JavaScript/leapYearCalc.js
@@ -1,7 +1,10 @@
 function isLeapYear(year) {
-
-    if (year / 4 && year % 4) {
-        
+    if(year % 4 === 0 && year % 100 == 0 && year % 400 === 0) {
+        return(year + " is a leap year.")
+    } else if(year % 4 === 0 && year % 100 !== 0) {
+        return(year + " is a leap year.")
     }
+    return(year + " is not a leap year.")
 }
-    
+
+console.log(isLeapYear(1700));
