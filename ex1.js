5209f1a02b38e513e56e681acb09cb3f8d1f2a85
NoChaps <vkempiak@gmail.com>
2023-03-07 00:16:35 -0500

    git commit

--- ex1.js
+++ ex1.js
@@ -1,49 +1,7 @@
-let output = [];
-let x = 1;
-function fizzBuzz() {
-
-    if (x % 3 === 0 && x % 5 === 0) {
-        output.push("FizzBuzz");
-        x++;
-        console.log(output);
-    } else if (x % 3 === 0) {
-        output.push("Fizz");
-        x++;
-        console.log(output);
-    } else if (x % 5 === 0) {
-        output.push("Buzz");
-        x++;
-        console.log(output);
-    } else {
-        output.push(x);
-        x++;
-        console.log(output);
-    }
+function whosPaying(names) {
+    let x = Math.floor(Math.random() * names.length);
+    return names[x] + " is going to buy lunch today!";
 }
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
+
+whosPaying("Angela", "Ben", "Jenny", "Michael", "Chloe");
+
