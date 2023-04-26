a963dae1ada12b4c4ac2d25775828259f14ebbae
NoChaps <vkempiak@gmail.com>
2023-04-26 17:22:28 -0400

    git commit

--- Projects/Simon Game/main.js
+++ Projects/Simon Game/main.js
@@ -1,82 +1,99 @@
-let buttonColors = ["red", "green", "yellow", "blue"];
+const buttonColors = ["red", "green", "yellow", "blue"];
 let gameOutput = [];
 let playerOutput = [];
-gameOutput.push(buttonColors[nextSequence()]);
-
 let currentLevel = 1;
-let userScore = 0;
-
-let red = document.getElementById('red');
-let green = document.getElementById('green');
-let yellow = document.getElementById('yellow');
-let blue = document.getElementById('blue');
-
-document.querySelectorAll(".btn").forEach(function(button) {
-    button.addEventListener("click", function() {
-        var color = button.getAttribute('id');
-        playerOutput.push(color);
-        playSound(color);
-        flashButton(button);
-    });
-});
-
-red.addEventListener('click', function() {
-    playerOutput.push(buttonColors[0]);
-    let audio = new Audio("sounds//red.mp3");
-    audio.play();
-    
-    let originalColor = red.style.backgroundColor;
-    red.style.backgroundColor = "white";
-    setTimeout(function() {
-        red.style.backgroundColor = originalColor;
-    }, 110);
-})
-green.addEventListener('click', function() {
-    playerOutput.push(buttonColors[1]);
-    let audio = new Audio("sounds//green.mp3");
-    audio.play();
-
-    let originalColor = green.style.backgroundColor;
-    green.style.backgroundColor = "white";
-    setTimeout(function() {
-        green.style.backgroundColor = originalColor;
-    }, 110);
-})
-yellow.addEventListener('click', function() {
-    playerOutput.push(buttonColors[2]);
-    let audio = new Audio("sounds//yellow.mp3");
-    audio.play();
-
-    let originalColor = yellow.style.backgroundColor;
-    yellow.style.backgroundColor = "white";
-    setTimeout(function() {
-        yellow.style.backgroundColor = originalColor;
-    }, 110);
-})
-blue.addEventListener('click', function() {
-    playerOutput.push(buttonColors[3]);
-    let audio = new Audio("sounds//blue.mp3");
-    audio.play();
-
-    let originalColor = blue.style.backgroundColor;
-    blue.style.backgroundColor = "white";
-    setTimeout(function() {
-        blue.style.backgroundColor = originalColor;
-    }, 110);
-})
+let gameOverFlag = false;
+let gameStartFlag = false;
 
 document.addEventListener("keydown", function gameStart() {
+    if (gameStartFlag) return;
+    gameStartFlag = true;
     document.querySelector("h1").innerHTML = "Level " + currentLevel;
-    console.log("hello");
-
-}, {once : true})
-
+    gameSequence();
+}, {once : true});
 
 function nextSequence() {
   return Math.floor(Math.random() * 4);
 }
 
+function gameOver() {
+    document.querySelector("h1").innerHTML = "Game over! Your score: " + currentLevel + ". Press any key to restart the game."
+    let originalColor = document.body.style.backgroundColor;
+    document.body.style.backgroundColor = "red";
+    setTimeout(function() {
+        document.body.style.backgroundColor = originalColor;
+    }, 110);
+    gameRunning = false;
+    gameOverFlag = true;
+    document.querySelectorAll(".btn").forEach(function(button) {
+        button.disabled = true;
+    });
+    document.addEventListener("keydown", gameRestart);
+}
 
+function gameSequence() {
+    gameRunning = true;
+    let nextColor = buttonColors[nextSequence()];
+    gameOutput.push(nextColor);
+    let lastIndex = gameOutput.length - 1;
 
+    setTimeout(function() {
+        playSound(gameOutput[lastIndex]);
+        flashButton(document.getElementById(gameOutput[lastIndex]));
+    }, 1000)
 
+    setTimeout(function() {
+        gameRunning = false;
+    }, 1000);
+}
 
+document.querySelectorAll(".btn").forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (gameRunning || gameOverFlag || !gameStartFlag) return;
+        let color = button.getAttribute('id');
+        playerOutput.push(color);
+        checkGame();
+        playSound(color);
+        flashButton(button);
+    });
+});
+
+function playSound(button) {
+    let audio = new Audio("sounds/" + button + ".mp3");
+    audio.play();
+}
+
+function flashButton(button) {
+    let originalColor = button.style.backgroundColor;
+    button.style.backgroundColor = "white";
+    setTimeout(function() {
+        button.style.backgroundColor = originalColor;
+    }, 110);
+}
+
+function checkGame() {
+    if (playerOutput.length === gameOutput.length) {
+        if (gameOutput.toString() === playerOutput.toString()) {
+            gameSequence();
+            currentLevel++;
+            document.querySelector("h1").innerHTML = "Level " + currentLevel;
+            playerOutput = [];
+        } 
+        else {
+            gameOver();
+        }
+    }
+    else if (playerOutput[playerOutput.length - 1] !== gameOutput[playerOutput.length - 1]) {
+        gameOver();
+    }         
+}
+
+function gameRestart() {
+    gameOutput = [];
+    playerOutput = [];
+    currentLevel = 1;
+    gameOverFlag = false;
+    gameStartFlag = false;
+    document.querySelector("h1").innerHTML = "Level " + currentLevel;
+    gameSequence();
+}
