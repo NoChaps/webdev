143322d57824689d83a6844e941e52915953240a
NoChaps <vkempiak@gmail.com>
2023-05-03 17:04:35 -0400

    git commit calculator app

---
+++ Projects/Calculator/calculator.js
@@ -0,0 +1,23 @@
+const express = require("express");
+cont bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+
+    let result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+app.listen(3000, function() {
+    console.log("server started on port 3000");
+});
