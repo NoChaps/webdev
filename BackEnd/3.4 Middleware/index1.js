3cd43ff9164394d17a40a680b7a875916a9f101a
NoChaps <vkempiak@gmail.com>
2023-09-27 16:46:06 -0400

    git commit

---
+++ BackEnd/3.4 Middleware/index1.js
@@ -0,0 +1,21 @@
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  console.log("You have entered " + req.body.street + " for street name, and " + req.body.pet + " for pet name.");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
